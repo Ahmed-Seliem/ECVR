422d468070026540ba65ab0f6fccabc0db44ac90
Ahmed Selim <ahmed.selim@intalio.com>
2026-03-02 10:52:09 +0200

    add .

---
+++ ECM.ReservationSystem/Controllers/Admin/DebugController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECM.ReservationSystem.Controllers.Admin
+{
+    [Authorize]
+    public class DebugController : Controller
+    {
+        public IActionResult Claims()
+        {
+            var claims = User.Claims
+                .Select(c => new { c.Type, c.Value })
+                .ToList();
+
+            return Json(claims);
+        }
+    }
+}
